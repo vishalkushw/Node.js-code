cacf59621427032e7f981eeff5bb577c2d918aa6
vishal kushwah <kushwahvishal2391@gmail.com>
2025-01-04 00:18:04 +0530

    node uppercase

---
+++ 12 nov node1 uppercase/index.js
@@ -0,0 +1,16 @@
+// const http=require("http");
+// const uc=require("uppercase");
+// http.createServer((req,res)=>{
+//     res.write("well come")
+//     res.write(uc(" We are cybrom student and i am from wardha mp "));
+//     res.end();
+// }).listen(8080);
+
+const http=require("http");
+const uc=require("uppercase");
+http.createServer((req,res)=>{
+    res.write("well come")
+    res.write(uc(" We are cybrom student"));
+    res.end();
+}).listen(8080);
+
